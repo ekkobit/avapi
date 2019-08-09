f5afdb49b9b37ffe9268f85a2d74d43bf129bb81
Ole Olaussen <ole@olaussen.io>
2019-08-09 12:15:45 +0800

    More pep8 errors fixed

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
+
 
 def license():
     with open('LICENSE') as f:
         return f.read()
 
+
 setup(name='avapi',
       version='0.1',
-      description='Convert json data files from Alpha Vantage to Pandas data frames.',
+      description='Convert json data files from Alpha Vantage to Pandas data \
+        frames.',
       long_description=readme(),
       long_description_content_type="text/markdown",
       classifiers=[
@@ -23,7 +27,7 @@ setup(name='avapi',
       author='Ole Olaussen',
       author_email='olemolaussen@gmail.com',
       license='MIT',
-      license_file = license(),
+      license_file=license(),
       packages=setuptools.find_packages(),
       test_suite='nose.collector',
       tests_require=['nose'],
