6ee36a7ab1f82aaec148a4edef82de0ff5606a4f
Ole Martin Olaussen <ole@ekkobit.com>
2019-08-29 11:24:30 +0800

    radthedocs build complaints issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.append(os.path.relpath('../../avapi/'))
-from avapi import __version__, __authors__, __email__
+# sys.path.append(os.path.relpath('../../avapi/'))
+# from avapi import __version__, __authors__, __email__
 
 
 # -- Project information -----------------------------------------------------
